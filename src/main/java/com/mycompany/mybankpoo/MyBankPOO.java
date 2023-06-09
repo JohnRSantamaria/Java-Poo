3682312c3289287592e3aa4ae0743cf0aec450bf
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-09 12:52:27 -0500

    firstCommit

---
+++ src/main/java/com/mycompany/mybankpoo/MyBankPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mybankpoo;
+
+/**
+ *
+ * @author ResetStoreX
+ */
+public class MyBankPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
